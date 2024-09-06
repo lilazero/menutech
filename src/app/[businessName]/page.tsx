a5e943e8b26df13e41fb3202982b02eb29fab717
lilazero <andililajal@gmail.com>
2024-09-06 22:38:17 +0200

    Refactor BusinessList rendering in [businessName]/page.tsx

--- src/app/[businessName]/page.tsx
+++ src/app/[businessName]/page.tsx
@@ -10,11 +10,11 @@ export default async ({ params }: { params: any }) => {
 
   return (
     <div>
-      {productListByBusinessName.map((product) => (
-        <div className='grid grid-flow-row grid-cols-4'>
+      <div className='grid grid-flow-row grid-cols-4'>
+        {productListByBusinessName.map((product) => (
           <ProduktetComponent key={product.productName} produktet={product} />
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
