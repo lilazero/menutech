4e76ebf794fff9769f2d1c54b965867297abdb08
lilazero <andililajal@gmail.com>
2024-09-10 14:36:17 +0200

    config files and changed to using pnpm

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'eu-central-1.storage.xata.sh',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'eu-central-1.xata.sh',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
