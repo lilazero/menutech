3ab371058b11fee328d593e52e914811b3d9de04
lilazero <andililajal@gmail.com>
2024-10-12 18:25:24 +0200

    Refactor Navbar component and add ThemeSelector

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,14 +1,16 @@
 import MaxWidthWrapper from './MaxWidthWrapper';
 import Link from 'next/link';
+import { ThemeSelector } from './ui/theme-picker';
 
 function Navbar() {
   return (
-    <nav className='sticky z-[0] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg  transition-all'>
+    <nav className='sticky z-[0] h-14 inset-x-0 top-0 w-full border-b border-gray-200 backdrop-blur-lg transition-all'>
       <MaxWidthWrapper>
         <div className='flex items-center justify-between  border-b h-14 border-zinc-200'>
           <Link href='/' className='z-40 flex font-semibold'>
             Menu<span className='text-red-800'>Tech</span>
           </Link>
+          <ThemeSelector />
         </div>
       </MaxWidthWrapper>
     </nav>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ import {
 } from '@clerk/nextjs';
 import './globals.css';
 import MaxWidthWrapper from './components/MaxWidthWrapper';
-
+import { ThemeProvider } from './components/theme-provider';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from '@vercel/analytics/react';
 import Navbar from './components/Navbar';
@@ -29,18 +29,25 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang='en'>
         <body className={inter.className}>
-          <MaxWidthWrapper>
-            <Navbar />
-            {children}
-          </MaxWidthWrapper>
-          <footer>
-            <SignedOut>
-              <SignInButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </footer>
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='system'
+            enableSystem
+            disableTransitionOnChange
+          >
+            <MaxWidthWrapper>
+              <Navbar />
+              {children}
+            </MaxWidthWrapper>
+            <footer>
+              <SignedOut>
+                <SignInButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </footer>
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
